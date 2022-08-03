6c28fedfd764e9e18cb8fa0877d130b47e644b1c
collin-miller <collin.miller@clearme.com>
2022-08-03 09:26:41 -0500

    feat(cli): completing cli shell

---
+++ bin/__main__.py
@@ -0,0 +1,6 @@
+"""Executable hello-world"""
+
+from example.main import main
+
+if __name__ == "__main__":
+    main()
---
+++ example/main.py
@@ -0,0 +1,13 @@
+"""Hello world CLI Logic"""
+
+import logging
+import os
+
+
+def main() -> None:
+    """Entrypoint"""
+    logging.basicConfig(
+        level=os.getenv("LOG_LEVEL", "INFO"),
+        format="%(levelname)s: %(message)s",
+    )
+    logging.info("Hello world!")
